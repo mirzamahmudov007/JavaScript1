ae2ba9e8098d2f54c411dfe18ee03a280afd95d6
mirzamahmudov007 <gayratjonmirzamahmudov@gmail.com>
2024-01-31 14:59:46 +0500

    sdewd

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
+
+let piyodaElement = document.querySelector(".piyoda");
+let velikodaElement = document.querySelector(".velik");
+let carElement = document.querySelector(".car");
+let airplaneElement = document.querySelector(".airplane");
+
+
+let piyodaPelement = document.createElement("p");
+
+let velikdPelement = document.createElement("p");
+
+let carPelement = document.createElement("p");
+
+let airplanePelement = document.createElement("p");
 function ozgartir() {
-    // Input elementini tanlash
     let inputElement = document.getElementById('malumotInput');
 
-    // Inputdan kelgan malumotni olish
     let malumot = inputElement.value;
+    
 
-    // Malumotni o'zgartirish (misol uchun kichik harflarga o'zgartiramiz)
-    let yangiMalumot = malumot.toLowerCase();
-
-    // Yangi malumotni inputga qo'yish
-    inputElement.value = yangiMalumot;
-    let  piyodaElement = document.querySelector(".piyoda");
-    let velikodaElement = document.querySelector(".velik");
-    let carElement = document.querySelector(".car");
-    let airplaneElement = document.querySelector(".airplane");
-
-    let piyodaPelement = document.createElement("p");
     piyodaPelement.innerHTML = malumot / (3.6) + " SOAT";
     piyodaPelement.classList.add("time");
     piyodaElement.append(piyodaPelement);
     
-    let velikdPelement = document.createElement("p");
     velikdPelement.innerHTML = malumot / 20.1 + " SOAT";
     velikdPelement.classList.add("time");
     velikodaElement.append(velikdPelement);
 
-    let carPelement = document.createElement("p");
     carPelement.innerHTML = malumot / 70 + " SOAT";
     carPelement.classList.add("time");
     carElement.append(carPelement);
 
-    let airplanePelement = document.createElement("p");
     airplanePelement.innerHTML = malumot / 800 + " SOAT";
     airplanePelement.classList.add("time");
     airplaneElement.append(airplanePelement);
